e46115bad3e3938055fe49425b7299c3e691194e
cairnie <cairnie@en1.supercali.net>
2015-08-12 21:53:27 +0100

    if the page has children, display those

--- single-fabric.php
+++ single-fabric.php
@@ -7,6 +7,30 @@
 
 get_header(); ?>
 
+<!-- if the page has children: we want to display them in a gallery
+  and suppress any DETAILS and THUMBNAIL that would otherwise be displayed on
+  a single-fabric page. -->
+
+  <?php
+  $id = $post->ID;
+  $args = array(
+		'post_parent' => $id,
+	);
+
+  if ($children = get_children( $args )){
+    echo '<p>this page has child pages</p>';
+    foreach ($children as $child){
+      $link = get_permalink($child->ID);
+      echo "<a href='$link'>";
+      echo get_the_post_thumbnail($child->ID, 'medium');
+      echo "<p>$child->post_title</p>";
+      echo "</a>";
+      print_r( $child );
+    }
+
+  } else { ?>
+
+
 <div class="row oatmeal sasha push-down-1">
   <div class="col-md-7 text-center">
     <?php
@@ -32,9 +56,10 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 
       <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+
     </div>
 </div>
-
+<?php } //end if ?>
 
 
 <?php get_footer(); ?>
