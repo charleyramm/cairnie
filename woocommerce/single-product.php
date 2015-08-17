2421067c166a5f47cf1e152880d21cf42d134fb3
Charley <charleyramm@gmail.com>
2015-08-17 21:59:48 +0100

    hiding custom fields display

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -13,6 +13,16 @@
   </div>
   <div class="col-md-5"><br>
 
+	<?php
+		/**
+		 * woocommerce_before_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
     <p><?php the_title(); ?></p>
 
     <?php while ( have_posts() ) : the_post(); ?>
@@ -21,10 +31,31 @@
 
         <p><br>DETAILS</p>
         <dl class="dl-horizontal">
-          <?php cairnie_the_attributes(); ?>
+          <?php //cairnie_the_attributes(); ?>
         </dl>
+
+		<?php wc_get_template_part( 'content', 'single-product' ); ?>
       
       <?php endwhile; // end of the loop. ?>
+
+<?php
+		/**
+		 * woocommerce_after_main_content hook
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+
+	<?php
+		/**
+		 * woocommerce_sidebar hook
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
+	?>
 
       <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
 
