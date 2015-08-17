a7de7ebcbbb35a4c44d918b8aa72c461a4fef094
Charley <charleyramm@gmail.com>
2015-08-17 20:55:15 +0100

    Single product using the correct template layout

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,48 +1,40 @@
-<?php
-/**
- * The Template for displaying all single products.
- *
- * Override this template by copying it to yourtheme/woocommerce/single-product.php
- *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     1.6.4
- */
+<?php get_header(); ?>
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
 
-get_header( 'shop' ); ?>
+<div class="row oatmeal push-down-1">
 
-<div class="row sasha push-down-1">
-  <div class="entry-content col-md-6">
+  <div class="col-md-7 text-center">
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php wc_get_template_part( 'content', 'single-product' ); ?>
-
-		<?php endwhile; // end of the loop. ?>
-
-	<?php
-		/**
-		 * woocommerce_after_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
+    <?php
+      if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+      	the_post_thumbnail('large');
+      }
+      ?><br><br>
   </div>
-</div>
-<?php get_footer( 'shop' ); ?>
+  <div class="col-md-5"><br>
+
+    <p><?php the_title(); ?></p>
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php the_content(); ?>
+
+        <p><br>DETAILS</p>
+        <dl class="dl-horizontal">
+          <?php cairnie_the_attributes(); ?>
+        </dl>
+      
+      <?php endwhile; // end of the loop. ?>
+
+      <?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+
+    </div>
+
+
+  </div><!-- close row --> 
+</div><!-- close the other thing, container -->
+
+<?php get_footer(); ?>
+
+
 
